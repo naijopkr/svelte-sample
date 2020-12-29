23f732362b9aedf4abc481d2fcd73b9e74ff5b7c
Ariel Barcellos <barcellos.ariel@gmail.com>
2020-12-29 18:02:07 -0500

    create first migrations and seeders

---
+++ initial_migration.sql
@@ -0,0 +1,13 @@
+BEGIN;
+
+    CREATE TABLE products (
+        id  SERIAL PRIMARY KEY,
+        ean VARCHAR(13) NOT NULL UNIQUE,
+        "name" VARCHAR(255) NOT NULL,
+        "description" TEXT,
+        price FLOAT NOT NULL,
+        createdAt TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(),
+        updatedAt TIMESTAMP WITH TIME ZONE
+    );
+
+END;
