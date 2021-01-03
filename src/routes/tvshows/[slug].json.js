a28ca569b9265cae1676aece2553b0ae6518e649
Ariel Barcellos <barcellos.ariel@gmail.com>
2021-01-03 18:08:22 -0500

    fix not loading tv shows index

--- src/routes/tvshows/[slug].json.js
+++ src/routes/tvshows/[slug].json.js
@@ -3,8 +3,12 @@ import { listCharacters } from '../../../controllers/CharacterController'
 export function get(req, res,next) {
     const { slug } = req.params
 
-    listCharacters(slug).then(data => {
-        res.setHeader('Content-Type', 'application/json')
-        res.end(data)
-    }).catch(error => console.log(error))
+    if (slug) {
+        listCharacters(slug).then(data => {
+            res.setHeader('Content-Type', 'application/json')
+            res.end(data)
+        }).catch(error => console.log(error))
+    } else {
+        next()
+    }
 }
